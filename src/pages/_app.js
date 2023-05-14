4886a976c6054fcfae25a0be3cf4b53b733c453b
Will <wkimble91@gmail.com>
2023-05-14 11:47:41 -0500

    fixed header tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name='viewport' content='viewport-fit=cover' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
       <Component
         allData={data}
