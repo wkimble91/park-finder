f3922e98aa6f6aab8929730bf7783d1e4fe63659
Will <mail@willkimble.com>
2023-09-08 11:48:12 -0500

    Fixed crossorigin tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,11 @@ export default function Document() {
         <link rel='manifest' href='site.webmanifest' />
         {/* --- Fonts --- */}
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
+        />
         <link
           href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;0,800;1,300;1,400;1,600;1,800&display=swap'
           rel='stylesheet'
@@ -45,7 +49,7 @@ export default function Document() {
           href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
           rel='stylesheet'
           integrity='sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM'
-          crossorigin='anonymous'
+          crossOrigin='anonymous'
         />
       </Head>
       <body>
